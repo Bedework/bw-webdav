90ddbd360ec286964e8d02ea306a6f7ec67631ed
Mike Douglass <douglm@rpi.edu>
2009-07-29 17:31:57 +0000

    Better exception handling for getPrincipal

--- server/src/edu/rpi/cct/webdav/servlet/common/PropFindMethod.java
+++ server/src/edu/rpi/cct/webdav/servlet/common/PropFindMethod.java
@@ -240,7 +240,16 @@ public class PropFindMethod extends MethodBase {
 
     openTag(WebdavTags.multistatus);
 
-    if (node != null) {
+    if (node == null) {
+      openTag(WebdavTags.response);
+
+      // XXX missing href
+      openTag(WebdavTags.propstat);
+      addStatus(HttpServletResponse.SC_NOT_FOUND, null);
+      closeTag(WebdavTags.propstat);
+
+      closeTag(WebdavTags.response);
+    } else {
       doNodeAndChildren(node, 0, depth);
     }
 
