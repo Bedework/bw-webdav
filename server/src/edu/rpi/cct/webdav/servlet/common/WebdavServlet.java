12df3582c3f71300fdfdb0a95f12bcd13200120a
Mike Douglass <douglm@rpi.edu>
2011-05-02 00:55:01 +0000

    Don't log auth info

--- server/src/edu/rpi/cct/webdav/servlet/common/WebdavServlet.java
+++ server/src/edu/rpi/cct/webdav/servlet/common/WebdavServlet.java
@@ -6,9 +6,9 @@
     Version 2.0 (the "License"); you may not use this file
     except in compliance with the License. You may obtain a
     copy of the License at:
-        
+
     http://www.apache.org/licenses/LICENSE-2.0
-        
+
     Unless required by applicable law or agreed to in writing,
     software distributed under the License is distributed on
     an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -399,6 +399,12 @@ public abstract class WebdavServlet extends HttpServlet
       while (names.hasMoreElements()) {
         String key = (String)names.nextElement();
         String val = req.getHeader(key);
+        if (key.toLowerCase().equals("authorization") &&
+            (val != null) &&
+            (val.toLowerCase().startsWith("basic"))) {
+          val = "Basic **********";
+        }
+
         log.debug("  " + key + " = \"" + val + "\"");
       }
 
