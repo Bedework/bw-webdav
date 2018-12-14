8012e93be9ea1c8a468f1377e5879acd1a2de10f
mike <mikeadouglass@gmail.com>
2018-12-14 00:34:43 -0500

    Logging changes

--- src/main/java/org/bedework/webdav/servlet/common/WebdavServlet.java
+++ src/main/java/org/bedework/webdav/servlet/common/WebdavServlet.java
@@ -18,6 +18,7 @@
 */
 package org.bedework.webdav.servlet.common;
 
+import org.bedework.util.logging.BwLogger;
 import org.bedework.util.logging.Logged;
 import org.bedework.util.servlet.io.CharArrayWrappedResponse;
 import org.bedework.util.xml.XmlEmit;
@@ -445,5 +446,20 @@ public abstract class WebdavServlet extends HttpServlet
       }
     } catch (Throwable t) {
     }
+  }
+
+  /* ====================================================================
+   *                   Logged methods
+   * ==================================================================== */
+
+  private BwLogger logger = new BwLogger();
+
+  @Override
+  public BwLogger getLogger() {
+    if ((logger.getLoggedClass() == null) && (logger.getLoggedName() == null)) {
+      logger.setLoggedClass(getClass());
+    }
+
+    return logger;
   }
 }
